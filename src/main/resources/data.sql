d19e0d6aafa4148a24938a5ea4b40e28333764b8
Fabrício Takaki <fabriciohtakaki@gmail.com>
2024-08-16 15:49:38 -0300

    feat: add data and schema sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO status_reserva (descricao) VALUES ('Confirmada');
+INSERT INTO status_reserva (descricao) VALUES ('Cancelada');
+INSERT INTO status_reserva (descricao) VALUES ('Pendente');
+
+INSERT INTO status_notificacao (descricao) VALUES ('Enviada');
+INSERT INTO status_notificacao (descricao) VALUES ('Falha');
+INSERT INTO status_notificacao (descricao) VALUES ('Pendente');
+
+INSERT INTO tipo_notificacao (descricao) VALUES ('Confirmação');
+INSERT INTO tipo_notificacao (descricao) VALUES ('Check-in');
+INSERT INTO tipo_notificacao (descricao) VALUES ('Check-out');
+
+INSERT INTO tipo_quarto (descricao) VALUES ('Single');
+INSERT INTO tipo_quarto (descricao) VALUES ('Double');
+INSERT INTO tipo_quarto (descricao) VALUES ('Family');
